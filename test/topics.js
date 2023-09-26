2ebf0091665e74da9ed2079329e0025252b0f3c2
kellyycha <65841983+kellyycha@users.noreply.github.com>
2023-09-26 01:44:37 -0400

    added a test for private topic

--- test/topics.js
+++ test/topics.js
@@ -73,10 +73,27 @@ describe('Topic\'s', () => {
                 title: topic.title,
                 content: topic.content,
                 cid: topic.categoryId,
+                isPrivate: topic.isPrivate,
             }, (err, result) => {
                 assert.ifError(err);
                 assert(result);
                 topic.tid = result.topicData.tid;
+                done();
+            });
+        });
+
+        it('should create a new private topic with proper parameters', (done) => {
+            topics.post({
+                uid: topic.userId,
+                title: topic.title,
+                content: topic.content,
+                cid: topic.categoryId,
+                isPrivate: true,
+            }, (err, result) => {
+                assert.ifError(err);
+                assert(result);
+                topic.tid = result.topicData.tid;
+                assert.strictEqual(result.topicData.isPrivate, true);
                 done();
             });
         });
@@ -354,6 +371,7 @@ describe('Topic\'s', () => {
                 title: topic.title,
                 content: topic.content,
                 cid: topic.categoryId,
+                isPrivate: topic.isPrivate,
             }, (err, result) => {
                 if (err) {
                     return done(err);
@@ -383,6 +401,7 @@ describe('Topic\'s', () => {
                 assert.strictEqual(topicData.deleted, 0);
                 assert.strictEqual(topicData.locked, 0);
                 assert.strictEqual(topicData.pinned, 0);
+                assert.strictEqual(topicData.isPrivate, false);
                 done();
             });
         });
